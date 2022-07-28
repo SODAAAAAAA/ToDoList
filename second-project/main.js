14365314421fbc2cd5444fabbc402d7efbfd609b
SODAAAAAAA <bsohee0206@gmail.com>
2022-07-28 17:28:24 +0900

    main logic

--- second-project/main.js
+++ second-project/main.js
@@ -10,14 +10,25 @@ let AddBtn = document.getElementById('AddBtn')
 let taskList = []
 let taskItem = document.querySelectorAll('.taskItem')
 
-AddBtn.addEventListener('click', addTask)
+let navUnderLine = document.getElementById('underLine')
+let navMene = document.querySelectorAll('.taskArea ul li a')
+
+AddBtn.addEventListener('click', addTask);
+navMene.forEach(menu=>
+    menu.addEventListener('click', (e) => navIndicator(e))
+);
+
+function navIndicator(e) {
+    navUnderLine.style.left = e.currentTarget.offsetLeft + 'px';
+    navUnderLine.style.width = e.currentTarget.offsetWidth + 'px';
+    navUnderLine.style.top = e.currentTarget.offsetHeight - 3 + 'px';
+}
 
 function addTask(){
     let task = {
         id: randomIDGenerate(),
         taskContent: taskInput.value,
-        isComplete: false,
-        delete: false
+        isComplete: false
     }
 
     taskList.push(task)
@@ -31,24 +42,14 @@ function render(){
 
     for(let i = 0; i<taskList.length; i++) {
 
-        if(taskList[i].delete == true) {
-
-            taskList.splice(i, 1);
-
-            resultHTML += ''
-
-            document.getElementById('taskBoard').removeChild(taskItem)
-
-            //아니 왜 두번 눌러야하느거임........ 
-
-        } else if(taskList[i].isComplete == true){
+        if(taskList[i].isComplete == true){
             resultHTML +=`<div class="taskItem">
             <div class="taskDone">
                 ${taskList[i].taskContent}
             </div>
             <div>
                 <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-arrow-rotate-right fa-lg"></i></button>
-                <button onclick="toggleDelete('${taskList[i].id}')"><i class="fa-solid fa-trash fa-lg"></i></button>
+                <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash fa-lg"></i></button>
             </div>
         </div>`
         } else {
@@ -58,7 +59,7 @@ function render(){
             </div>
             <div>
                 <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-check fa-lg"></i></button>
-                <button onclick="toggleDelete('${taskList[i].id}')"><i class="fa-solid fa-trash fa-lg"></i></button>
+                <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash fa-lg"></i></button>
             </div>
         </div>`;
         }
@@ -67,9 +68,6 @@ function render(){
     document.getElementById('taskBoard').innerHTML = resultHTML;
 }
 
-function deleteItem() {
-    console.log("삭제 가보자고")
-}
 
 function toggleComplete(id){
 
@@ -83,12 +81,11 @@ function toggleComplete(id){
     render();
 }
 
-function toggleDelete(id){
-
+function deleteTask(id){
     for(let i = 0; i<taskList.length; i++) {
         if(taskList[i].id == id) {
-            taskList[i].delete = !taskList[i].delete;
-            break;
+            taskList.splice(i, 1);
+            break
         }
     }
 
